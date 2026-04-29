158e0bfcc79779f54d4ba23f3ab0cd4503e75045
victorybu <141458417+victorybu@users.noreply.github.com>
2026-04-29 17:19:47 -0400

    force dynamic setup

--- wowsgnl-work/app/api/setup/route.ts
+++ wowsgnl-work/app/api/setup/route.ts
@@ -1,6 +1,8 @@
 import { initSchema } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     await initSchema();
